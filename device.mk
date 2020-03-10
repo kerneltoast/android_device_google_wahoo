e9f9c15d35bac8a499d012d0609cf7dacb6798e9
Eamon Powell <eamonpowell@outlook.com>
2020-03-10 13:39:19 +1100

    wahoo: Build SystemUIGoogle
    Change-Id: I641373b6b6cc72f43bab75750c726963eca40646

--- device.mk
+++ device.mk
@@ -768,3 +768,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # MiFare Permissions file
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
+
+# Google Device
+PRODUCT_PACKAGES += \
+    SystemUIGoogle
