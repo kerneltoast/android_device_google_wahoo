0e2fed2f74e3bd7c696ca8baa8c0698743a19909
Eamon Powell <eamonpowell@outlook.com>
2020-04-14 17:35:18 +1000

    AICP-ify
    Change-Id: I85af67b8c5c028a7c226f5bd3cb0f72cff396bfd

--- device.mk
+++ device.mk
@@ -721,3 +721,49 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/fstab.postinstall:$(TARGET_COPY_OUT_PRODUCT)/etc/fstab.postinstall
+
+# EUICC feature
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
+
+# IMS
+PRODUCT_PACKAGES += \
+    com.android.ims.rcsmanager \
+    RcsService \
+    PresencePolling
+
+# Enable missing props
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.opa.eligible_device=true
+
+# Snap
+PRODUCT_PACKAGES += \
+    Snap
+
+# Build vendor img
+AB_OTA_PARTITIONS += \
+    vendor
+
+# Build necessary packages for vendor
+PRODUCT_PACKAGES += \
+    ese-ls-provision \
+    ese-replay \
+    libjson \
+    libsensorndkbridge:32 \
+    libtinyxml
+
+# Add missing vendor props
+PRODUCT_PROPERTY_OVERRIDES += \
+    drm.service.enabled=true \
+    media.mediadrmservice.enable=true \
+    ro.oem_unlock.pst_offset=6144 \
+    ro.oem_unlock.pst=/dev/block/platform/soc/1da4000.ufshc/by-name/misc \
+    ro.hardware.egl=adreno \
+    ro.gfx.driver.0=com.google.pixel.wahoo.gfxdrv
+
+# Setting vendor SPL
+VENDOR_SECURITY_PATCH := "2020-04-05"
+
+# MiFare Permissions file
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
